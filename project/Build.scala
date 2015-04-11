30efdc19944bade26298a4cdc05eed7813b6a958
Nicolas Voirol <voirol.nicolas@gmail.com>
2015-04-11 18:16:52 +0200

    Setup project to point to correct devsearch-ast

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ object DevSearchLearning extends Build {
 
   lazy val root = Project("root", file(".")).dependsOn(astParser % "compile->compile;test->test")
 
-  lazy val astCommit = "f2681ef71cbf68722825b7ab50c13cb4143f75a8"
-  //lazy val astParser = RootProject(uri(s"git://github.com/devsearch-epfl/devsearch-ast.git#$astCommit"))
-  lazy val astParser = RootProject(file("../devsearch-ast"))
+  lazy val astCommit = "55a86c047a14526d856bf197fda7112d1ff9a470"
+  lazy val astParser = RootProject(uri(s"git://github.com/devsearch-epfl/devsearch-ast.git#$astCommit"))
+  // lazy val astParser = RootProject(file("../devsearch-ast"))
 }
