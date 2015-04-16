b262c9b879d16f5a657a2bbbf52cb385f6887485
hubi <nicolas.hubacher@epfl.ch>
2015-04-16 21:54:30 +0200

    Works now perfectly on local machine, but throws strange error on cluster.

--- src/main/scala/devsearch/FeatureMining.scala
+++ src/main/scala/devsearch/FeatureMining.scala
@@ -1,6 +1,8 @@
 package devsearch
 
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 
 
 
@@ -10,26 +12,41 @@ import org.apache.spark.{SparkConf, SparkContext}
  */
 object FeatureMining {
 
-  //TODO: SET PATH HERE!
+  //TODO: SET PATH TO CORRECT REPOSITORY DIRECTORY HERE!
   //val inputDir = "/projects/devsearch/repositories/*"
-  //val inputDir  = "/projects/devsearch/testrepos/java/*"
-  //val outputDir = "/projects/devsearch/features"
-  val inputDir =  "/home/hubi/Documents/BigData/DevSearch/testrepos/java/bla/*"
-  val outputDir = "/home/hubi/Documents/BigData/DevSearch/features"
+  val inputDir  = "/projects/devsearch/testrepos"
+  val outputDir = "/projects/devsearch/features"
+  //val inputDir =  "/home/hubi/Documents/BigData/DevSearch/testrepos"
+  //val outputDir = "/home/hubi/Documents/BigData/DevSearch/features"
 
-
+  /**
+   * We need to process BLOB by BLOB because the header line of each file gets collected in AstExtractor.
+   * Since the BLOBs are so huge, it is still running in parallel.
+   * @param args
+   */
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("FeatureMining")
     implicit val sc = new SparkContext(conf)
 
 
+    val fs = FileSystem.get(new java.net.URI(inputDir + "/*"), new Configuration())
+    val fileList = fs.listStatus(new Path(inputDir))
+                     .map(_.getPath)                    //these are all the language directories...
+                     .flatMap(p => fs.listStatus(p))    //these are all the files in the language directories
+                     .map(_.getPath.toString)
 
-    val codeFiles = AstExtractor extract inputDir
 
-    val features = CodeEater eat codeFiles
 
-    println("\n\n\n\n\n\n\n\nGenerated "+features.count()+ " features from " + codeFiles.count + " files.\n\n\n\n\n\n\n")
+    for(inputFile <- fileList){
+      println("\n\n\n\n\n\n\n\nProcessing "+ inputFile +"\n\n\n\n\n\n\n\n")
 
-    features map(_.toString) saveAsTextFile(outputDir)
+      val codeFiles = AstExtractor extract inputFile
+
+      val features = CodeEater eat codeFiles
+
+      println("\n\n\n\n\n\n\n\nGenerated "+features.count()+ " features from " + codeFiles.count + " files.\n\n\n\n\n\n\n\n")
+
+      features map(_.toString) saveAsTextFile(outputDir)
+    }
   }
 }
