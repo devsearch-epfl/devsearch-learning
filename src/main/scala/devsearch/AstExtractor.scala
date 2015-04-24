454b90be05e18cc497951bcb5ef6fed6917b6c56
Pierre Walch <pwalch@gmail.com>
2015-04-24 19:20:16 +0200

    Use regex matching only for header.

--- src/main/scala/devsearch/AstExtractor.scala
+++ src/main/scala/devsearch/AstExtractor.scala
@@ -6,27 +6,26 @@ import org.apache.hadoop.io.Text
 import org.apache.spark.rdd._
 import scala.util.parsing.combinator._
 
-case class NoAstCodeFile(size: Long, language: String, codeFileLocation: CodeFileLocation, code: String) extends java.io.Serializable
+case class NoAstCodeFile(metadata: CodeFileMetadata, content: Text) extends java.io.Serializable
+case class CodeFileMetadata(size: Long, language: String, codeFileLocation: CodeFileLocation) extends java.io.Serializable
 
-object SnippetParser extends RegexParsers with java.io.Serializable {
+object HeaderParser extends RegexParsers with java.io.Serializable {
   val number: Parser[String] = """[\n]?\d+""".r
-  val noSlash: Parser[String] = """[^/]+""".r
-  val path: Parser[String] = """[^\n]+""".r
-  // Everything until end of line
-  val code: Parser[String] = """(?s).*""".r // the rest
+  val noSlashRegex: Parser[String] = """[^/]+""".r
+  val pathRegex: Parser[String] = """[^\n]+""".r
 
-  def parseBlob: Parser[NoAstCodeFile] = (
-    number ~ ":../data/crawld/java/" ~ noSlash ~ "/" ~ noSlash ~ "/" ~ path ~ code ^^ {
-        case size ~ _ ~ owner ~ _ ~ repo ~ _ ~ path ~ code =>
-          NoAstCodeFile(size.replace("\n", "").toLong, "Java", CodeFileLocation(owner, repo, path), code)
+  def parseBlob: Parser[CodeFileMetadata] = (
+    number ~ ":../data/crawld/java/" ~ noSlashRegex ~ "/" ~ noSlashRegex ~ "/" ~ pathRegex ^^ {
+        case size ~ _ ~ owner ~ _ ~ repo ~ _ ~ path =>
+          CodeFileMetadata(size.replace("\n", "").toLong, "Java", CodeFileLocation(owner, repo, path))
     }
-    | number ~ ":Go/" ~ noSlash ~ "/" ~ noSlash ~ "/" ~ path ~ code ^^ {
-        case size ~ _ ~ owner ~ _ ~ repo ~ _ ~ path ~ code =>
-          NoAstCodeFile(size.toLong, "Go", CodeFileLocation(owner, repo, path), code)
+    | number ~ ":Go/" ~ noSlashRegex ~ "/" ~ noSlashRegex ~ "/" ~ pathRegex ^^ {
+        case size ~ _ ~ owner ~ _ ~ repo ~ _ ~ path =>
+          CodeFileMetadata(size.toLong, "Go", CodeFileLocation(owner, repo, path))
     }
-    | number ~ ":Scala/" ~ noSlash ~ "/" ~ noSlash ~ "/" ~ path ~ code ^^ {
-        case size ~ _ ~ owner ~ _ ~ repo ~ _ ~ path ~ code =>
-          NoAstCodeFile(size.toLong, "Scala", CodeFileLocation(owner, repo, path), code)
+    | number ~ ":Scala/" ~ noSlashRegex ~ "/" ~ noSlashRegex ~ "/" ~ pathRegex ^^ {
+        case size ~ _ ~ owner ~ _ ~ repo ~ _ ~ path =>
+          CodeFileMetadata(size.toLong, "Scala", CodeFileLocation(owner, repo, path))
     }
   )
 }
@@ -35,18 +34,16 @@ object AstExtractor {
   val MEGABYTES = Math.pow(2, 20)
 
   def extract(files: RDD[(Text, Text)]): RDD[CodeFileData] = {
-    files.map {
-      case (headerLine, content) => headerLine + "\n" + content
-    }.flatMap { snippet =>
-      val result = SnippetParser.parse(SnippetParser.parseBlob, snippet)
+    files.flatMap { case (headerLine, content) =>
+      val result = HeaderParser.parse(HeaderParser.parseBlob, headerLine.toString)
       if (result.isEmpty) None
-      else Some(result.get)
-    }.filter { case NoAstCodeFile(size, _, _, _) =>
-      size <= 10 * MEGABYTES
-    }.map { case NoAstCodeFile(size, language, codeFileLocation, code) =>
-        CodeFileData(size, language, codeFileLocation, code)
-    }.filter(c =>
-      c.ast != NoDef
+      else Some(NoAstCodeFile(result.get, content))
+    }.filter { case NoAstCodeFile(metadata, content) =>
+      metadata.size <= 10 * MEGABYTES
+    }.map { case NoAstCodeFile(metadata: CodeFileMetadata, content: Text) =>
+      CodeFileData(metadata.size, metadata.language, metadata.codeFileLocation, content.toString)
+    }.filter(codeFile =>
+      codeFile.ast != NoDef
     )
   }
 }
