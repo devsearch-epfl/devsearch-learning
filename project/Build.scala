c459865f49e21196a1ab29230523781f69a1c236
Pierre Walch <pwalch@gmail.com>
2015-04-29 00:15:15 +0200

    Update devsearch-ast project reference.

--- project/Build.scala
+++ project/Build.scala
@@ -4,6 +4,6 @@ object DevSearchLearning extends Build {
 
   lazy val root = Project("root", file(".")).dependsOn(astParser % "compile->compile;test->test")
 
-  lazy val astCommit = "d6cc37be5e084d88f97f10cfb8fc8cde9149db42"
+  lazy val astCommit = "8465455a756a36255ac67ab24936b1e4471641d8"
   lazy val astParser = RootProject(uri(s"git://github.com/devsearch-epfl/devsearch-ast.git#$astCommit"))
 }
