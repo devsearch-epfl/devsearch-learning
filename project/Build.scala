79adbb4dac3f160648cb72b3822d495b8711fa13
Pierre Walch <pwalch@gmail.com>
2015-05-01 21:21:10 +0200

    Guess extension instead of using major language.

--- project/Build.scala
+++ project/Build.scala
@@ -4,6 +4,6 @@ object DevSearchLearning extends Build {
 
   lazy val root = Project("root", file(".")).dependsOn(astParser % "compile->compile;test->test")
 
-  lazy val astCommit = "771da00198bef249f68813aa7fd67913f0c1e700"
+  lazy val astCommit = "af4a968fa551b186f4c4f5eb6f9db60432da2826"
   lazy val astParser = RootProject(uri(s"git://github.com/devsearch-epfl/devsearch-ast.git#$astCommit"))
 }
