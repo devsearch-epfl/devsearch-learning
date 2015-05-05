0a7956c2766ea923a2423d195e378f0b80d848b8
Pierre Walch <pwalch@gmail.com>
2015-05-06 01:58:12 +0200

    Fix MiningTest.

--- src/test/scala/devsearch/spark/MiningTest.scala
+++ src/test/scala/devsearch/spark/MiningTest.scala
@@ -1,11 +1,18 @@
 package devsearch.spark
 
-import devsearch.features.{Feature, FeatureRecognizer}
+import devsearch.features._
 import org.scalatest.FlatSpec
 
 class MiningTest extends FlatSpec {
   "Feature mining" should "extract some features" in {
     val featureSet = AstExtractor.extract(Utility.headerSnippetPairs).flatMap(FeatureRecognizer).collect.toSet
-    assert(Set[Feature]().subsetOf(featureSet))
+    assert(
+      Set[Feature](
+        TypeRefFeature(
+          CodePiecePosition(CodeFileLocation("fwbrasil", "activate", "SlickQueryContext.scala"), 42),
+          "JdbcRelationalStorage"
+        )
+      ).subsetOf(featureSet)
+    )
   }
 }
