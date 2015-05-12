9990ad6cc7f68be10e449011eaf7e8103019296e
Pierre Walch <pwalch@gmail.com>
2015-05-13 00:54:32 +0200

    Catch OutOfMemoryError.

--- project/Build.scala
+++ project/Build.scala
@@ -4,6 +4,6 @@ object DevSearchLearning extends Build {
 
   lazy val root = Project("root", file(".")).dependsOn(astParser % "compile->compile;test->test")
 
-  lazy val astCommit = "a9f86ef9b379fc9e549c50148eaaef352c3e7e13"
+  lazy val astCommit = "4835466aa21e11675f5b37442f403122e8a97312"
   lazy val astParser = RootProject(uri(s"git://github.com/devsearch-epfl/devsearch-ast.git#$astCommit"))
 }
